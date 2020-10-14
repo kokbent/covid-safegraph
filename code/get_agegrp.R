92057dc6bac4d6ba80a05a464366ee1937326c30
Kok Ben Toh <kokbent@ufl.edu>
2020-10-13 22:53:10 -0400

    Add work_behave noteen version

--- code/get_agegrp.R
+++ code/get_agegrp.R
@@ -90,6 +90,40 @@ get_agegrp1 <- function (cbg) {
   return(cbg_agegrp)
 }
 
+#### Noteens
+get_agegrp_nt <- function (cbg) {
+  tmp <- cbg %>%
+    select(GEOID10, starts_with("AGE_")) %>%
+    select(-AGE_65_UP)
+  tmp <- st_set_geometry(tmp, NULL)
+  colnames(tmp)[colnames(tmp) == "AGE_UNDER5"] <- "AGE_0_5"
+  colnames(tmp)[ncol(tmp)] <- "AGE_85_120"
+  
+  tmp1 <- tmp %>%
+    pivot_longer(cols = -GEOID10,
+                 names_to = c("age_lo", "age_hi"),
+                 names_pattern = "AGE_(.*)_(.*)",
+                 values_to = "freq") %>%
+    group_by(GEOID10) %>%
+    mutate(prop = freq / sum(freq))
+  
+  cbg_agegrp <- tmp1 %>%
+    mutate(age_grp = case_when(
+      age_hi <= 17 ~ "00 - 17",
+      age_hi <= 29 ~ "18 - 29",
+      age_hi <= 49 ~ "30 - 49",
+      age_hi <= 64 ~ "50 - 64",
+      TRUE         ~ "65 and above"
+    )) %>%
+    filter(age_grp != "00 - 17") %>%
+    group_by(GEOID10, age_grp) %>%
+    summarise(n = sum(freq)) %>%
+    group_by(GEOID10) %>%
+    mutate(prop = n/sum(n))
+  
+  return(cbg_agegrp)
+}
+
 #### Categorize dist trav
 cat_dist_trav <- function (d) {
   if (d == 1) return(c("0", "1-1000", "1001-2000"))
